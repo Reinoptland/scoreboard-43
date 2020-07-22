0ec0ed78bc887a390fa13eb5a792b43587b4d951
Reinoptland <reinoptland@gmail.com>
2020-07-22 16:25:17 +0200

    Add event listener / handler and update the state for score

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,10 +4,21 @@ export default function Player(props) {
   console.log("WHAT ARE PROPS?", props);
   const [score, setScore] = useState(0);
 
+  // event handler
+  function incrementScore() {
+    console.log("GIVE THIS PERSON A POINT!", props.name);
+    const newScore = score + 1;
+    // call the "setter" to update the state
+    setScore(newScore);
+  }
   return (
     <div>
       <h2>Player: {props.name}</h2>
       <p>Score: {score}</p>
+      <div>
+        {/* add onClick event listener */}
+        <button onClick={incrementScore}>+</button>
+      </div>
     </div>
   );
 }
