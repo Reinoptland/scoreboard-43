80b457a1e9334221d238d1ae14803319b6f40071
Reinoptland <reinoptland@gmail.com>
2020-07-22 16:14:54 +0200

    Make Player component, use in ScoreBoard

---
+++ src/components/Player.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Player() {
+  return (
+    <div>
+      <h2>Player</h2>
+    </div>
+  );
+}
--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Player from "./Player";
 
 export default function ScoreBoard() {
   return (
     <div>
       <h1>I AM A SCOREBOARD</h1>
+      <Player />
+      <Player />
+      <Player />
+      <Player />
     </div>
   );
 }
